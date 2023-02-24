20c3f49d1319e732b94e8182d4cdf53a9219b99d
iOS <ios-work-station@ios.local>
2023-02-24 11:38:56 +0330

    my initial fuckinnnnggg commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by iOS on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
